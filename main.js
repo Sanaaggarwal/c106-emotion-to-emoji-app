de344790979f977bbab5af983342783498b233d8
Sanaaggarwal <72506943+Sanaaggarwal@users.noreply.github.com>
2021-02-22 19:38:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,4 +22,38 @@ function speak(){
     utter=new SpeechSynthesisUtterance(speakdata1+speakdata2);
     synth.speak(utter);
 }
- 
+ function identifyimage(){
+    img=document.getElementById("capturedimage");
+    classifier.classify(img,getresult);
+ }
+ function getresult(error,result){
+   if (error){
+     console.log(error);
+   }
+   else{
+     console.log(result);
+     Prediction1=result[0].label;
+     prediction2=result[1].label;
+     document.getElementById("result_emotion_name").innerHTML=Prediction1;
+     document.getElementById("result_emotion_name2").innerHTML=prediction2;
+     speak();
+     if(Prediction1=="happy"){
+       document.getElementById("update_emoji").innerHTML="&#128522;";
+     }
+     if(Prediction1=="sad"){
+      document.getElementById("update_emoji").innerHTML="&#128532;";
+    }
+    if(Prediction1=="angry"){
+      document.getElementById("update_emoji").innerHTML="&#128548;";
+    }
+    if(prediction2=="happy"){
+      document.getElementById("update_emoji2").innerHTML="&#128522;";
+    }
+    if(prediction2=="sad"){
+     document.getElementById("update_emoji2").innerHTML="&#128532;";
+   }
+   if(prediction2=="angry"){
+     document.getElementById("update_emoji2").innerHTML="&#128548;";
+   }
+   }
+ }
